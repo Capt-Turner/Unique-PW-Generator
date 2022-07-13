6bb56ad2d9fa8b9f03b680f1d3022270a11492fa
Seth Turner <sethhet@gmail.com>
2022-07-12 21:38:44 -0600

    working product and updated README

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -1,12 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-PW ={length:8,
+PW ={length:0,
   Lcase:false,
   Ucase:false,
   numeral:false,
   special:false};
 
-// define attributes of password through promtps, alerts when invalid inputs
+
 function writePassword() {
   var passwordText = document.querySelector("#password");
   let length = prompt("How long would you like your password?" , 8);
@@ -16,10 +16,9 @@ function writePassword() {
   if (length > 128){
     alert("Cannot allow more than 128 characters. Please restart.");
   }
-  // if (length.type !== 'number'){
-  //   alert("Only numbers are accepted. Please restart.");
-  // } breaks it for some reason
+
   else {
+    PW.length = length;
     PW.Lcase = confirm("Do you want lowercase letters in your password?");
     PW.Ucase = confirm("Do you want uppercase letters in your password?");
     PW.numeral = confirm("Do you want numbers in your password?");
@@ -28,9 +27,8 @@ function writePassword() {
   }
 function generatePassword() {
   const choice = [];
-  const pass = [];
   if (PW.Lcase == true){
-  choice.push("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
+    choice.push("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
   }
   if (PW.Ucase == true){
     choice.push("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z")
@@ -41,18 +39,18 @@ function generatePassword() {
   if (PW.special == true){
     choice.push(" ","!","@",'"',"#","$","%","&","'","(",")","*","+","-",".","/",":",";","<",">","?","[","]","^","_","`","{","}","|","~")
   }
-  // const pw = characters[Math.floor(Math.random() * characters.length)];
-  // passwordText.textContent = pw;
-  // for (let i = 0; i < characters.length; i++) {
-  //    pass.push(Math.floor(Math.random() * PW.length))
-  // };
-  // passwordText.textContent = pass;
+  for (let i = 0; i < `${PW.length}`; i++) {
+    const pass = choice[Math.floor(Math.random() * choice.length)];
+    passwordText.textContent += pass;
+  }
+
   console.log(PW);
   console.log(choice);
+
    }};
 
 
-// take attributes and plug into function to create password
+
 
 
 generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 Follow along with the in-browser prompts to create and customize a password between 8 and 128 characters long, upper and/or lower case type, numerals, and/or special characters. 
 
 You must select at least one type of character or it will fail.
+
+If all steps are completed properly, the newly generated password will appear in the text box for convienient copy and pasteing
+
+<img src="./assets/screenshots/Example.png" height=500px width=500px>
